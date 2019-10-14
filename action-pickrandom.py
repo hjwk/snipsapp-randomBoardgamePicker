2cc7e94841929fbd484044fc5f360e61f3c6a145
Hubert Woszczyk <woszczykh@gmail.com>
2019-10-14 18:56:00 +0200

    extracted api related code

--- action-pickrandom.py
+++ action-pickrandom.py
@@ -3,11 +3,12 @@
 from snipsTools import SnipsConfigParser
 from hermes_python.hermes import Hermes
 
-import requests
-import random
-
 # imported to get type check and IDE completion
 from hermes_python.ontology.dialogue.intent import IntentMessage
+
+from api import Api
+import json
+
 
 CONFIG_INI = "config.ini"
 
@@ -23,6 +24,10 @@ required_slots_questions = {
     "num_players": "Ok, mais pour combien de joueurs ?",
 }
 
+required_slots = {
+    "num_players": None
+}
+
 class PickRandomBoardgame(object):
     """class used to wrap action code with mqtt connection
        please change the name refering to your application
@@ -33,45 +38,50 @@ class PickRandomBoardgame(object):
         try:
             self.config = SnipsConfigParser.read_configuration_file(CONFIG_INI)
         except Exception:
-            print("WHAT!")
             self.config = None
 
-        # get authentication token
-        payload = { 'username': self.config.get("secret").get("username"), 'password': self.config.get("secret").get("password") }
-        resp = requests.post(self.config.get("global").get("backend_api") + '/user/login', data=payload)
-
-        if not resp.ok:
-            print("ERROR")
-
-        self.token = resp.json()['token']
+        self.apiHandler = Api(self.config.get("secret").get("username"),
+                              self.config.get("secret").get("password"),
+                              self.config.get("global").get("backend_api"))
 
         self.start_blocking()
 
     @staticmethod
     def PickRandomBoardgameCallback(hermes: Hermes, intent_message: IntentMessage):
-        return hermes.publish_end_session(intent_message.session_id, "Ok")
+        available_slots = json.loads(intent_message.custom_data)
+        
+        num_players_slot = intent_message.slots.num_players.first().value or available_slots["num_players"]
+
+        available_slots["num_players"] = num_players_slot
+
+        if not num_players_slot:
+            return hermes.publish_continue_session(intent_message.session_id,
+                                                    required_slots_questions["num_players"],
+                                                    ["hjwk:ElicitNumPlayers"],
+                                                    custom_data=json.dumps(available_slots))
+        
+        hermes.publish_end_session(intent_message.session_id, "Ok, laisse moi un instant...")
 
     @staticmethod
-    def ElicitNumPlayersCallback(
-                                hermes: Hermes,
-                                intent_message: IntentMessage):
+    def ElicitNumPlayersCallback(hermes: Hermes, intent_message: IntentMessage):
 
         # terminate the session first if not continue
-        hermes.publish_end_session(intent_message.session_id, "Bien reçu")
+        hermes.publish_end_session(intent_message.session_id, "D'accord, laisse moi réfléchir...")
 
         # action code goes here...
         print('[Received] intent: {}'.format(
             intent_message.intent.intent_name))
 
         # if need to speak the execution result by tts
-        hermes.publish_start_session_notification(
-            intent_message.site_id,
+        hermes.publish_start_session_notification(intent_message.site_id,
             "Action 2", "Je vais vous trouver ça")
 
     # register callback function to its intent and start listen to MQTT bus
     def start_blocking(self):
         with Hermes(MQTT_ADDR) as h:
-            h.subscribe_intent("hjwk:PickRandomBoardgame", self.PickRandomBoardgameCallback).loop_forever()
+            h.subscribe_intent("hjwk:PickRandomBoardgame", self.PickRandomBoardgameCallback)
+            h.subscribe_intent('hjwk:ElicitNumPlayers', self.ElicitNumPlayersCallback)
+            h.loop_forever()
 
 if __name__ == "__main__":
     PickRandomBoardgame()
---
+++ api.py
@@ -0,0 +1,35 @@
+import requests
+import random
+
+class Api(object):
+    def __init__(self, username, password, backend_url):
+        # get authentication token
+        payload = { 'username': username, 'password': password }
+        resp = requests.post(backend_url + '/user/login', data=payload)
+
+        if not resp.ok:
+            print("ERROR")
+
+        self.token = resp.json()['token']
+        self.username = username
+        self.password = password
+        self.backend_url = backend_url
+
+    def getRandomBoardgames(self, numberOfPlayers):
+        ## Get boardgames
+        url = self.backend_url + '/user/current/library_games'
+        library = requests.get(url, headers={'Authentication': 'JWT ' + self.token})
+        games = library.json()
+
+        ## Filter according to number of players
+        filteredGames = []
+        for game in games:
+            if (game['board_game']['min_players'] <= numberOfPlayers and
+                game['board_game']['max_players'] >= numberOfPlayers):
+                filteredGames.append(game)
+
+        if len(filteredGames) <= 3:
+            return [ filteredGames[i]['board_game']['name'] for i in range(len(filteredGames)) ]
+        else:
+            option1, option2, option3 = random.sample(range(0, len(filteredGames)), 3)
+            return [ filteredGames[i]['board_game']['name'] for i in [option1, option2, option3] ]
