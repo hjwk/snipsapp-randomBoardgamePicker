6fbbc9f51474d8e9404a80821e0fae93b1323094
Hubert Woszczyk <woszczykh@gmail.com>
2019-10-22 16:52:23 +0200

    Added numberOfPossessedGames intent

--- action-pickrandom.py
+++ action-pickrandom.py
@@ -20,18 +20,15 @@ required_slots_questions = {
 
 class PickRandomBoardgame(object):
     def __init__(self):
-        # get the configuration if needed
         try:
             self.config = SnipsConfigParser.read_configuration_file(CONFIG_INI)
+            self.apiHandler = Api(self.config.get("secret").get("username"),
+                              self.config.get("secret").get("password"),
+                              self.config.get("global").get("backend_api"))
         except Exception:
             self.config = None
 
-        self.apiHandler = Api(self.config.get("secret").get("username"),
-                              self.config.get("secret").get("password"),
-                              self.config.get("global").get("backend_api"))
-
         self.numberOfBoardgames = 3
-
         self.start_blocking()
 
     def PickRandomBoardgameCallback(self, hermes: Hermes, intent_message: IntentMessage):
@@ -70,8 +67,10 @@ class PickRandomBoardgame(object):
         answer = "Vous pourriez jouer à "
         for i in range(len(boardgames)):
             answer += boardgames[i]
-            if i < len(boardgames) - 1:
-                answer += " ou à, "
+            if i < len(boardgames) - 2:
+                answer += ", "
+            elif i == len(boardgames) - 2:
+                answer += " ou à "
 
         hermes.publish_start_session_notification(intent_message.site_id, answer, "")
 
@@ -80,12 +79,18 @@ class PickRandomBoardgame(object):
         favouriteBoardgame = self.apiHandler.getMostPlayedBoardgame()
         hermes.publish_start_session_notification(intent_message.site_id, "Votre jeu préféré est " + favouriteBoardgame, "")
 
+    def PossessedBoardgames(self, hermes: Hermes, intent_message: IntentMessage):
+        hermes.publish_end_session(intent_message.session_id, "")
+        numberOfOwnedBoardgames = self.apiHandler.getNumberOfBoardgames()
+        hermes.publish_start_session_notification(intent_message.site_id, "Vous possédez " + str(numberOfOwnedBoardgames), "")
+
     # register callback function to its intent and start listen to MQTT bus
     def start_blocking(self):
         with Hermes(MQTT_ADDR) as h:
             h.subscribe_intent("hjwk:PickRandomBoardgame", self.PickRandomBoardgameCallback)
             h.subscribe_intent('hjwk:ElicitNumPlayers', self.ElicitNumPlayersCallback)
             h.subscribe_intent('hjwk:FavouriteBoardgame', self.FavouriteBoardgame)
+            h.subscribe_intent('hjwk:PossessedBoardgames', self.PossessedBoardgames)
             h.loop_forever()
 
 if __name__ == "__main__":
--- api.py
+++ api.py
@@ -12,9 +12,13 @@ class Api(object):
 
         self.boardgames = None
 
+    def authorizedGetRequest(self, url):
+        return requests.get(url, headers={'Authentication': 'JWT ' + self.token})
+
     def getToken(self):
         payload = { 'username': self.username, 'password': self.password }
-        resp = requests.post(self.backend_url + '/user/login', data=payload)
+        url = self.backend_url + '/user/login'
+        resp = requests.post(url, data=payload)
 
         if not resp.ok:
             print("Token could not be retrieved")
@@ -24,18 +28,22 @@ class Api(object):
 
     def getUserId(self):
         url = self.backend_url + '/user/current'
-        user = requests.get(url,  headers={'Authentication': 'JWT ' + self.token})
+        user = self.authorizedGetRequest(url)
+
+        if not user.ok:
+            print("UserId could not be retrieved")
+            return None
 
         return user.json()["id"]
 
     def updateLibrary(self):
         if not self.boardgames:
             url = self.backend_url + '/user/current/library_games'
-            library = requests.get(url, headers={'Authentication': 'JWT ' + self.token})
+        library = self.authorizedGetRequest(url)
             if not library.ok:
                 print("Token no longer valid, refreshing")
                 self.token = self.getToken()
-                library = requests.get(url, headers={'Authentication': 'JWT ' + self.token})
+            library = self.authorizedGetRequest(url)
 
             self.boardgames = library.json()
 
@@ -50,7 +58,7 @@ class Api(object):
                 game['board_game']['max_players'] >= numberOfPlayers):
                 filteredGames.append(game['board_game']['name'])
 
-        numberOfBoardgames = numberOfBoardgames if numberOfBoardgames > 0 else  3
+        numberOfBoardgames = numberOfBoardgames if numberOfBoardgames > 0 else 3
 
         if len(filteredGames) < numberOfBoardgames:
             return filteredGames
@@ -61,7 +69,15 @@ class Api(object):
     def getMostPlayedBoardgame(self):
         # Get stats
         url = self.backend_url + '/user/' + str(self.userId) + '/stats'
-        stats = requests.get(url, headers={'Authentication': 'JWT ' + self.token})
+        stats = self.authorizedGetRequest(url)
 
         if stats.ok:
             return stats.json()['most_played']['board_game']['name']
+
+    def getNumberOfBoardgames(self):
+        #Get stats
+        url = self.backend_url + '/user/' + str(self.userId) + '/stats'
+        stats = self.authorizedGetRequest(url)
+
+        if stats.ok:
+            return stats.json()['owned']
